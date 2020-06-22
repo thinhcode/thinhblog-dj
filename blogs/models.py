5cb9106c676dd77f085294ceb2691b3f1ee17898
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2020-06-23 00:44:09 +0700

    translation model

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Ad(models.Model):
@@ -9,59 +10,85 @@ class Ad(models.Model):
         ('3', '728x90px'),
         ('4', '300x600px'),
     ]
-    style = models.CharField(verbose_name='Định dạng', max_length=1, choices=AD_STYLE)
-    content = models.TextField(verbose_name='Code')
-    publish = models.BooleanField(verbose_name='Công khai', default=True)
+    style = models.CharField(_('Định dạng'), max_length=1, choices=AD_STYLE)
+    html_code = models.TextField()
+
+    class Meta:
+        verbose_name = _('Quảng cáo')
+        verbose_name_plural = _('Quảng cáo')
 
     def __str__(self):
         return self.get_style_display()
 
 
 class Author(models.Model):
-    user = models.OneToOneField(verbose_name='Người dùng', to=User, on_delete=models.CASCADE)
-    display_name = models.CharField(verbose_name='Tên hiển thị', max_length=50)
-    avatar = models.ImageField(verbose_name='Ảnh đại diện', upload_to='blogs/%Y/%m', blank=True)
+    user = models.OneToOneField(to=User, verbose_name=_('Người dùng'), on_delete=models.CASCADE)
+    display_name = models.CharField(_('Tên hiển thị'), max_length=50)
+    avatar = models.ImageField(_('Ảnh đại diện'), upload_to='blogs/%Y/%m', blank=True)
+
+    class Meta:
+        verbose_name = _('Người dùng')
+        verbose_name_plural = _('Người dùng')
 
     def __str__(self):
         return self.display_name
 
 
 class Category(models.Model):
-    title = models.CharField(verbose_name='Tiêu đề', max_length=200)
-    slug = models.SlugField(verbose_name='Đường dẫn')
+    title = models.CharField(_('Tiêu đề'), max_length=200)
+    slug = models.SlugField(_('Đường dẫn'))
+
+    class Meta:
+        verbose_name = _('Danh mục')
+        verbose_name_plural = _('Danh mục')
 
     def __str__(self):
         return self.title
 
 
 class Page(models.Model):
-    title = models.CharField(verbose_name='Tiêu đề', max_length=200)
-    slug = models.SlugField(verbose_name='Đường dẫn')
-    description = models.CharField(verbose_name='Mô tả', max_length=300, blank=True)
-    content = models.TextField(verbose_name='Nội dung', blank=True)
+    title = models.CharField(_('Tiêu đề'), max_length=200)
+    slug = models.SlugField(_('Đường dẫn'))
+    description = models.CharField(_('Mô tả'), max_length=300, blank=True)
+    content = models.TextField(_('Nội dung'), blank=True)
+
+    class Meta:
+        verbose_name = _('Trang')
+        verbose_name_plural = _('Trang')
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    title = models.CharField(verbose_name='Tiêu đề', max_length=200)
-    slug = models.SlugField(verbose_name='Đường dẫn')
-    author = models.ForeignKey(verbose_name='Tác giả', to=Author, on_delete=models.CASCADE, default=1)
-    description = models.CharField(verbose_name='Mô tả', max_length=300, blank=True)
-    content = models.TextField(verbose_name='Nội dung', blank=True)
-    categories = models.ForeignKey(verbose_name='Danh mục', to=Category, on_delete=models.SET_NULL, null=True)
-    tags = models.CharField(verbose_name='Thẻ', max_length=200, help_text='Cách nhau bởi dấu ,', blank=True)
-    thumbnail = models.ImageField(verbose_name='Ảnh bìa', upload_to='blogs/%Y/%m', help_text='350x200px', blank=True)
-    publish = models.BooleanField(verbose_name='Công khai', default=True)
+    title = models.CharField(_('Tiêu đề'), max_length=200)
+    slug = models.SlugField(_('Đường dẫn'))
+    author = models.ForeignKey(to=Author, verbose_name=_('Tác giả'), on_delete=models.CASCADE, default=1)
+    description = models.CharField(_('Mô tả'), max_length=300, blank=True)
+    content = models.TextField(_('Nội dung'), blank=True)
+    categories = models.ForeignKey(to=Category, verbose_name=_('Danh mục'), on_delete=models.SET_NULL, null=True)
+    tags = models.CharField(_('Thẻ'), max_length=200, help_text='Cách nhau bởi dấu ,', blank=True)
+    thumbnail = models.ImageField(_('Ảnh bìa'), upload_to='blogs/%Y/%m', help_text='350x200px', blank=True)
+    publish = models.BooleanField(_('Công khai'), default=True)
+
+    class Meta:
+        verbose_name = _('Bài viết')
+        verbose_name_plural = _('Bài viết')
 
     def __str__(self):
         return self.title
 
 
 class Setting(models.Model):
-    ads = models.ManyToManyField(verbose_name='Quảng cáo', to=Ad, blank=True)
+    ads = models.ManyToManyField(to=Ad, verbose_name=_('Quảng cáo'), blank=True)
     google_code = models.TextField(blank=True)
+    facebook = models.URLField(blank=True)
+    email = models.EmailField(max_length=50, blank=True)
+    github = models.URLField(blank=True)
+
+    class Meta:
+        verbose_name = _('Cài đặt')
+        verbose_name_plural = _('Cài đặt')
 
     def __str__(self):
-        return 'Cài đặt'
+        return str(_('Thiết lập'))
