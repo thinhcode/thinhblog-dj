96addfd16eae25454f49d5c2b0b6ef8faa26b87c
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2020-07-08 01:39:26 +0700

    add generic context mixin

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import F, Q
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import base, ListView, DetailView, View
 from django.utils.translation import gettext_lazy as _
 from .models import Ad, Category, Post, Setting
 
@@ -8,24 +8,26 @@ from .models import Ad, Category, Post, Setting
 def get_ad_queryset(id: int):
     return Ad.objects.filter(style__exact=str(id)).last()
 
-def get_general_context():
-    context = dict()
-    context['categories'] = Category.objects.all()
-    context['ad_300x250'] = get_ad_queryset(1)
-    context['ad_468x60'] = get_ad_queryset(2)
-    context['ad_728x90'] = get_ad_queryset(3)
-    context['ad_300x600'] = get_ad_queryset(4)
 
-    setting_obj = Setting.objects
-    if setting_obj.exists():
-        context['facebook'] = setting_obj.values().last().get('facebook')
-        context['email'] = setting_obj.values().last().get('email')
-        context['github'] = setting_obj.values().last().get('github')
-    
-    return context
+# Generic Context Mixin
+class GenCtxMixin(base.ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['ad_300x250'] = get_ad_queryset(1)
+        context['ad_468x60'] = get_ad_queryset(2)
+        context['ad_728x90'] = get_ad_queryset(3)
+        context['ad_300x600'] = get_ad_queryset(4)
+
+        setting_obj = Setting.objects
+        if setting_obj.exists():
+            context['facebook'] = setting_obj.values().last().get('facebook')
+            context['email'] = setting_obj.values().last().get('email')
+            context['github'] = setting_obj.values().last().get('github')
+        return context
 
 
-class SearchView(ListView):
+class SearchView(GenCtxMixin, ListView):
     model = Post
     template_name = 'blogs/index.html'
     ordering = ['-timestamp']
@@ -44,12 +46,11 @@ class SearchView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update(get_general_context())
         context['search_query'] = self.request.GET.get('q', '')
         return context
 
 
-class CategoryListView(ListView):
+class CategoryListView(GenCtxMixin, ListView):
     model = Post
     template_name = 'blogs/index.html'
     ordering = ['-timestamp']
@@ -63,26 +64,16 @@ class CategoryListView(ListView):
             category_list = category_list.order_by(*ordering)
         return category_list
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update(get_general_context())
-        return context
 
-
-class PostListView(ListView):
+class PostListView(GenCtxMixin, ListView):
     model = Post
     template_name = 'blogs/index.html'
     ordering = ['-timestamp']
     page_kwarg = _('trang')
     paginate_by = 2
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update(get_general_context())
-        return context
 
-
-class PostDetailView(DetailView):
+class PostDetailView(GenCtxMixin, DetailView):
     model = Post
     template_name = 'blogs/single.html'
 
@@ -91,8 +82,3 @@ class PostDetailView(DetailView):
         post.views = F('views') + 1
         post.save()
         return super().get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update(get_general_context())
-        return context
