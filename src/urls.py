bb601797e2dee15d6b18fd6894429e0262356fcd
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2020-08-09 12:44:12 +0700

    add favicon.ico

--- src/urls.py
+++ src/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('favicon.ico/', RedirectView.as_view(url='/static/blogs/img/favicon.ico')),
     path('', include('blogs.urls')),
 ]
 
